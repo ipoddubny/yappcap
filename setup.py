9ae2c8f70e8776f278ae51ad41e1dd0d2869d76a
George Joseph <gjoseph@sangoma.com>
2022-05-12 07:55:25 -0600

    Create release 0.0.1

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
-from distutils.core import setup
 import os
+
+VERSION = '0.0.1'
 
 if not os.path.exists('definitions.pxi'):
     os.system('make definitions.pxi')
 
-setup(
-    ext_modules=cythonize('yappcap.pyx')
+    setup(
+    name='yappcap',
+    version=VERSION,  
+    ext_modules=cythonize('yappcap.pyx'),
+    install_requires=['wheel', 'setuptools', 'Cython'],
+    packages=find_packages(),
+    url='https://github.com/asterisk/yappcap',
 )
