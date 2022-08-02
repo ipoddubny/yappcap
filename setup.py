4fda582d2b9a116bc40684fa2f724dcd949ced45
George Joseph <gjoseph@sangoma.com>
2022-08-02 11:09:28 -0600

    Convert to pyproject.toml
    Since every distribution and kernel we support has AF_LINK
    and AF_PACKET, we can now skip the need to generate the
    definitions.pxi file and just check it in.

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from Cython.Build import cythonize
-import os
 
-VERSION = '0.0.1'
-
-if not os.path.exists('definitions.pxi'):
-    os.system('make definitions.pxi')
-
-    setup(
-    name='yappcap',
-    version=VERSION,  
-    ext_modules=cythonize('yappcap.pyx'),
-    install_requires=['wheel', 'setuptools', 'Cython'],
-    packages=find_packages(),
-    url='https://github.com/asterisk/yappcap',
+setup(
+    ext_modules = cythonize("yappcap.pyx")
 )
